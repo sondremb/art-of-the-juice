e8156256bcd694acafeb3f462dfcead69a01011d
Sondre Bakke <sondremartin@hotmail.com>
2025-04-01 21:32:36 +0200

    SpriteComponent er nå en Component

--- core/src/main/kotlin/dev/bakke/artofjuice/components/SpriteComponent.kt
+++ core/src/main/kotlin/dev/bakke/artofjuice/components/SpriteComponent.kt
@@ -2,17 +2,13 @@ package dev.bakke.artofjuice.components
 
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import dev.bakke.artofjuice.Entity
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import ktx.graphics.use
 
-class SpriteComponent(val sprite: Sprite) {
-    fun update(delta: Float) {
-        // Update sprite
-    }
-
-    fun render(entity: Entity, spriteBatch: SpriteBatch) {
+class SpriteComponent(val sprite: Sprite) : Component() {
+    override fun render(batch: SpriteBatch, shape: ShapeRenderer) {
         // Render sprite
-        spriteBatch.use {
+        batch.use {
             it.draw(sprite, entity.position.x, entity.position.y)
         }
     }
