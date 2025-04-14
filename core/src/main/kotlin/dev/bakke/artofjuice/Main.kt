d97f28d55d296c32b844a47234b193f872743c35
Sondre Bakke <sondremartin@hotmail.com>
2025-04-14 13:20:56 +0200

    Rename og flytt screen

--- core/src/main/kotlin/dev/bakke/artofjuice/Main.kt
+++ core/src/main/kotlin/dev/bakke/artofjuice/Main.kt
@@ -1,140 +1,17 @@
 package dev.bakke.artofjuice
 
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Input
-import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Pixmap
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.graphics.glutils.FrameBuffer
-import com.badlogic.gdx.graphics.glutils.ShaderProgram
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer
-import com.badlogic.gdx.maps.objects.RectangleMapObject
-import com.badlogic.gdx.maps.tiled.TiledMap
-import com.badlogic.gdx.maps.tiled.TmxMapLoader
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
-import com.badlogic.gdx.math.Matrix4
-import com.badlogic.gdx.math.Rectangle
-import dev.bakke.artofjuice.collision.shapes.RectangleCollisionShape
-import dev.bakke.artofjuice.collision.ColliderComponent
-import dev.bakke.artofjuice.components.PhysicsComponent
-import dev.bakke.artofjuice.collision.CollisionSystem
-import dev.bakke.artofjuice.player.*
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
-import ktx.app.clearScreen
-import ktx.assets.disposeSafely
-import ktx.assets.toInternalFile
 import ktx.async.KtxAsync
-import ktx.graphics.use
-import ktx.inject.Context
-import ktx.math.vec2
-import ktx.tiled.x
-import ktx.tiled.y
 
 class Main : KtxGame<KtxScreen>() {
     override fun create() {
         KtxAsync.initiate()
 
-        addScreen(FirstScreen())
+        addScreen(GameScreen())
         addScreen(PerlinTest())
         addScreen(ConfigGunVisualsScreen())
-        setScreen<FirstScreen>()
+        setScreen<GameScreen>()
     }
 }
 
-class FirstScreen : KtxScreen {
-    private val batch = SpriteBatch()
-    private val shape = ShapeRenderer()
-    private val context = Context()
-    private val world = World(context)
-    private val collisionSystem = CollisionSystem().apply { context.bindSingleton(this) }
-    private val gunVisualsManager = GunVisualsManager().apply { loadJson() }
-    private val player = world.spawnEntity(vec2(100f, 100f)) {
-        +Tag.PLAYER
-        +PhysicsComponent(-900f)
-        +PlayerInputComponent()
-        +PlayerVisuals()
-        +ColliderComponent(RectangleCollisionShape(Rectangle(0f, 0f, 24f, 32f)))
-        +GunComponent(null)
-        +GunInventoryComponent()
-        +GrenadeThrowerComponent()
-    }
-    private val enemySpawner = world.spawnEntity(vec2(0f, 0f)) {
-        +SpawnEnemyComponent(0.8f)
-    }
-    private val camera = OrthographicCamera()
-    private val screenshakeSystem = ScreenshakeSystem(camera, player).apply { context.bindSingleton(this) }
-    private val debugUI = DebugUI(batch, player)
-    private lateinit var map: TiledMap
-    private lateinit var renderer: OrthogonalTiledMapRenderer
-    lateinit var frameBuffer: FrameBuffer
-    lateinit var shader: ShaderProgram
-    lateinit var shaderBatch: SpriteBatch
-
-
-    override fun show() {
-        frameBuffer = FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.width, Gdx.graphics.height, false)
-        shader = ShaderProgram("shaders/shockwave.vert".toInternalFile(), "shaders/shockwave.frag".toInternalFile())
-
-        if (!shader.isCompiled) {
-            Gdx.app.error("Shader", "Compilation failed:\n" + shader.log)
-        }
-
-        shaderBatch = SpriteBatch(1000, shader)
-        map = TmxMapLoader().load("map.tmx")
-        player.position = map.layers.get("Player").objects.get("Spawn").let { vec2(it.x, it.y) }
-        renderer = OrthogonalTiledMapRenderer(map)
-        camera.setToOrtho(false, 800f, 600f) // Adjust to match your game window size
-        map.layers.get("Player").objects.get("Enemy").let { enemySpawner.position.set(it.x, it.y) }
-
-        val layer = map.layers.get("metal_collision")
-        layer.objects
-            .map { (it as RectangleMapObject).rectangle }
-            .forEach { collisionSystem.addTerrainCollider(RectangleCollisionShape(it)) }
-    }
-
-    override fun render(delta: Float) {
-        world.update(delta)
-        collisionSystem.update(delta)
-        camera.position.set(player.position.x, player.position.y, 0f)
-        screenshakeSystem.update(delta)
-        batch.projectionMatrix = camera.combined
-        shape.projectionMatrix = camera.combined
-        renderer.setView(camera)
-        frameBuffer.use {
-            clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
-            renderer.render()
-            world.render(batch, shape)
-            collisionSystem.render(batch, shape)
-
-            if (Gdx.input.isKeyJustPressed(Input.Keys.F1)) {
-                GamePreferences.setRenderDebug(!GamePreferences.renderDebug())
-            }
-
-            if (GamePreferences.renderDebug()) {
-                debugUI.render(delta)
-                screenshakeSystem.render(batch, shape)
-            }
-        }
-
-        shaderBatch.projectionMatrix = Matrix4().setToOrtho2D(0f, 0f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
-        shaderBatch.use {
-            it.draw(frameBuffer.colorBufferTexture, 0f, 0f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat(), 0f, 0f, 1f, 1f)
-        }
-    }
-
-    override fun resize(width: Int, height: Int) {
-        camera.viewportWidth = width.toFloat()
-        camera.viewportHeight = height.toFloat()
-        camera.update()
-    }
-
-    override fun dispose() {
-        map.disposeSafely()
-        renderer.disposeSafely()
-        batch.disposeSafely()
-        shape.disposeSafely()
-        debugUI.disposeSafely()
-        gunVisualsManager.disposeSafely()
-    }
-}
