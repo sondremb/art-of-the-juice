9e22dfb6762a9312806e228e61351ad3e6f29334
Sondre Bakke <sondremartin@hotmail.com>
2025-04-16 10:37:51 +0200

    Litt mer camera greier

--- core/src/main/kotlin/dev/bakke/artofjuice/CameraComponent.kt
+++ core/src/main/kotlin/dev/bakke/artofjuice/CameraComponent.kt
@@ -1,26 +1,53 @@
 package dev.bakke.artofjuice
 
 import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.Rectangle
 import dev.bakke.artofjuice.engine.Entity
 import dev.bakke.artofjuice.engine.components.Component
-import ktx.math.minus
+import dev.bakke.artofjuice.engine.gdx.extensions.rect
+import ktx.graphics.use
 import ktx.math.plus
-import ktx.math.times
 import ktx.math.vec2
 
 class CameraComponent(private var camera: Camera, private var player: Entity) : Component() {
+    private val deadzone = Rectangle(-20f, -10f, 40f, 20f)
+    private val minHeight = 16f
+    private val moveSpeedX = 0.05f
+    private val moveSpeedY = 0.1f
     override fun update(delta: Float) {
-        val target = player.position + vec2(0f, 72f)
-        val minHeight = 16f
-
+        var target = player.position + vec2(0f, 72f)
+        deadzone.setCenter(entity.position)
+        if (target.x > deadzone.x && target.x < deadzone.x + deadzone.width) {
+            target.x = entity.position.x
+        }
+        if (target.y > deadzone.y && target.y < deadzone.y + deadzone.height) {
+            target.y = entity.position.y
+        }
         val halfHeight = camera.viewportHeight / 2f
         // vil ikke ha nedre camera grense lavere enn minHeight ->
-        // vil ikke ha target - halfHeigt < minHeight ->
+        // vil ikke ha target - halfHeight < minHeight ->
         // vil ikke ha target < minHeight + halfHeight
         target.y = target.y.coerceAtLeast(minHeight + halfHeight)
 
-        entity.position += (target - entity.position) * 0.1f
+        entity.position.x += (target.x - entity.position.x) * moveSpeedX
         camera.position.x = entity.position.x
+        entity.position.y += (target.y - entity.position.y) * moveSpeedY
         camera.position.y = entity.position.y
     }
+
+    override fun render(batch: SpriteBatch, shape: ShapeRenderer) {
+        val cameraMinY = minHeight + camera.viewportHeight / 2f
+        val rawTarget = player.position + vec2(0f, 72f)
+        if (GamePreferences.renderDebug()) {
+            shape.use(ShapeRenderer.ShapeType.Line) {
+                it.rect(rawTarget.x, rawTarget.y, 1f, 1f)
+                it.line(-100f, cameraMinY, 1000f, cameraMinY)
+                deadzone.setCenter(entity.position)
+                it.rect(deadzone)
+            }
+        }
+    }
+
 }
