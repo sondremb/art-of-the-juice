f039c41a8a311d26678865e9034644e5df25328c
Sondre Bakke <sondremartin@hotmail.com>
2025-05-12 18:53:29 +0200

    Refactor the b u l l e t

--- core/src/main/kotlin/dev/bakke/artofjuice/gun/Bullet.kt
+++ core/src/main/kotlin/dev/bakke/artofjuice/gun/Bullet.kt
@@ -1,38 +1,29 @@
 package dev.bakke.artofjuice.gun
 
 import com.badlogic.gdx.graphics.g2d.Animation
-import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.math.Vector2
 import dev.bakke.artofjuice.Assets
 import dev.bakke.artofjuice.HealthComponent
 import dev.bakke.artofjuice.Tag
 import dev.bakke.artofjuice.TextureAssets
 import dev.bakke.artofjuice.engine.AnimationRenderable
+import dev.bakke.artofjuice.engine.Entity
 import dev.bakke.artofjuice.engine.ParticleSystem
 import dev.bakke.artofjuice.engine.collision.ColliderComponent
 import dev.bakke.artofjuice.engine.components.Component
 import dev.bakke.artofjuice.engine.components.PhysicsComponent
-import ktx.assets.toInternalFile
 import ktx.math.vec2
 
 class BulletComponent(private val gunStats: GunStats) : Component() {
     override fun lateInit() {
-        val assets = getSystem<Assets>()
         val colliderComponent = getComponent<ColliderComponent>()
         colliderComponent.onCollision += {
             if (it.hasTag(Tag.ENEMY)) {
-                it.getComponent<HealthComponent>().damage(gunStats.damage)
-                it.getComponent<PhysicsComponent>().applyImpulse(vec2(entity.velocity.x, entity.velocity.x * 0.2f), gunStats.impulse)
-                val animation = assets.getRegions(TextureAssets.Effects.random())
-                    .let { Animation(1/24f, it) }
-                getSystem<ParticleSystem>().spawn(AnimationRenderable(animation), entity.position.cpy(), Vector2.Zero.cpy(), 0.2f)
-                entity.destroy()
+                onEnemyHit(it)
             }
         }
         colliderComponent.onTerrainCollision += {
-            val animation = assets.getRegions(TextureAssets.Effects.random())
-                .let { Animation(1/24f, it) }
-            getSystem<ParticleSystem>().spawn(AnimationRenderable(animation), entity.position.cpy(), Vector2.Zero.cpy(), 0.2f)
+            spawnParticleEffect()
             entity.destroy()
         }
     }
@@ -40,5 +31,19 @@ class BulletComponent(private val gunStats: GunStats) : Component() {
     override fun update(delta: Float) {
         entity.position.x += entity.velocity.x * delta
         entity.position.y += entity.velocity.y * delta
+    }
+
+    private fun onEnemyHit(enemy: Entity) {
+        enemy.getComponent<HealthComponent>().damage(gunStats.damage)
+        enemy.getComponent<PhysicsComponent>().applyImpulse(vec2(entity.velocity.x, entity.velocity.x * 0.2f), gunStats.impulse)
+        spawnParticleEffect()
+        entity.destroy()
+    }
+
+    private fun spawnParticleEffect() {
+        val assets = getSystem<Assets>()
+        val animation = assets.getRegions(TextureAssets.Effects.random())
+            .let { Animation(1/24f, it) }
+        getSystem<ParticleSystem>().spawn(AnimationRenderable(animation), entity.position.cpy(), Vector2.Zero.cpy(), 0.2f)
     }
 }
